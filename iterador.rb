065cff792af983fe249e9ce4ad3460ef431e7432
BayronZ <bayron.zavalag@gmail.com>
2022-03-25 19:06:50 -0300

    Finished: iterador.rb

---
+++ iterador.rb
@@ -0,0 +1,17 @@
+#En el siguiente código, reemplazar la instrucción while por times dentro del programa
+# llamado iterador.rb
+
+
+#El siguiente codigo
+# i = 0
+# while i < 50
+#     puts "Iteración #{i}"
+#     i = i + 1
+# end
+
+50.times do |i|
+    puts "Iteración #{i}"
+
+end
+
+# 50.times {|i| puts "Iteración #{i}"}
